a92b82cd9c16a3a08b3f3234910b30f284a532fd
caglarcercinli <chaglar.ronin@gmail.com>
2021-05-04 16:50:36 +0200

    eerste push

---
+++ src/main/java/be/vdab/fietsen2/Fietsen2Application.java
@@ -0,0 +1,13 @@
+package be.vdab.fietsen2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fietsen2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fietsen2Application.class, args);
+    }
+
+}
